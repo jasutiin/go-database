9f0fc5e2618fb6703ba96fc58da9a681fd5c9f40
Justine <justinemangaliman@gmail.com>
2026-08-13 20:53:18 -0600

    skip list constructor

--- skiplist/skip_list.go
+++ skiplist/skip_list.go
@@ -12,6 +12,18 @@ type SkipList[T any] struct {
 	head     *Node[T]
 	level    int
 	maxLevel int
+	compare  func(a, b T) int
+}
+
+func NewSkipList[T any](maxLevel int, compare func(a, b T) int) *SkipList[T] {
+	return &SkipList[T]{
+		head: &Node[T]{
+			next: make([]*Node[T], maxLevel),
+		},
+		level:    0,
+		maxLevel: maxLevel,
+		compare:  compare,
+	}
 }
 
 func (s *SkipList[T]) Insert(node *Node[T]) error {
