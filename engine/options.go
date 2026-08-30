484d1a7e680af2743476ea5dc1571dd7068314e2
Justine <justinemangaliman@gmail.com>
2026-08-29 18:38:57 -0600

    validate max size and size properly for opts

--- engine/options.go
+++ engine/options.go
@@ -9,20 +9,20 @@ type Options struct {
 }
 
 var ErrDbNameRequired = errors.New("DbName is required for options")
-var ErrMaxLevelBelowZero = errors.New("Skip List max level cannot be less than zero")
-var ErrMaxSizeBelowZero = errors.New("Skip List max size cannot be less than zero")
+var ErrMaxLevelLTEZero = errors.New("Skip List max level cannot be less than or equal to zero")
+var ErrMaxSizeLTEZero = errors.New("Skip List max size cannot be less than or equal to zero")
 
 func (opts *Options) Validate() error {
 	if opts.DbName == "" {
 		return ErrDbNameRequired
 	}
 
-	if opts.SkipListMaxLevel < 0 {
-		return ErrMaxLevelBelowZero
+	if opts.SkipListMaxLevel <= 0 {
+		return ErrMaxLevelLTEZero
 	}
 
-	if opts.SkipListMaxSize < 0 {
-		return ErrMaxSizeBelowZero
+	if opts.SkipListMaxSize <= 0 {
+		return ErrMaxSizeLTEZero
 	}
 
 	return nil
