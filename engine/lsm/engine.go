6bc40e6922e47f53fdee83e8e8e7882dbcf91cd8
Justine <justinemangaliman@gmail.com>
2026-08-30 11:03:23 -0600

    change project structure to support b-tree implementation

---
+++ engine/lsm/engine.go
@@ -0,0 +1,68 @@
+package lsm
+
+import "fmt"
+
+type Engine struct {
+	table         *memTable
+	writeAheadLog *wal
+	sstables      []*sst
+}
+
+func Startup(opts *Options) (*Engine, error) {
+	if err := opts.validate(); err != nil {
+		return nil, err
+	}
+
+	engine := &Engine{}
+	if err := engine.recover(opts); err != nil {
+		return nil, err
+	}
+
+	return engine, nil
+}
+
+func (engine *Engine) recover(opts *Options) error {
+	log, err := LoadWAL(opts)
+	if err != nil {
+		return err
+	}
+
+	table, err := LoadMemTable(opts, log)
+	if err != nil {
+		log.file.Close()
+		return err
+	}
+
+	engine.table = table
+	engine.writeAheadLog = log
+	return nil
+}
+
+func (engine *Engine) Stop() error {
+	if engine.writeAheadLog == nil || engine.writeAheadLog.file == nil {
+		return nil
+	}
+
+	return engine.writeAheadLog.file.Close()
+}
+
+func (engine *Engine) Get(key []byte) error {
+	fmt.Println("Get() not implemented")
+	return nil
+}
+
+func (engine *Engine) Put(key, value []byte) error {
+	if err := engine.writeAheadLog.Insert(key, value, false); err != nil {
+		return err
+	}
+
+	return engine.table.Insert(key, value, false)
+}
+
+func (engine *Engine) Delete(key []byte) error {
+	if err := engine.writeAheadLog.Insert(key, nil, true); err != nil {
+		return err
+	}
+
+	return engine.table.Insert(key, nil, true)
+}
--- engine/lsm/sst.go
+++ engine/lsm/sst.go
@@ -1,4 +1,4 @@
-package engine
+package lsm
 
 import "os"
 
